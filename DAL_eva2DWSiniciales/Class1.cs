4865fa4cff890f4f9eb65a94fa175daea0d50cd4
DESKTOP-QMGOQCG\CSI21-Amorlez <csi21-amorlez@colegioaltair.net>
2022-12-15 10:56:10 +0100

    Commit Inicial

---
+++ DAL_eva2DWSiniciales/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace DAL_eva2DWSiniciales
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ eva2DWSiniciales/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace eva2DWSiniciales.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
